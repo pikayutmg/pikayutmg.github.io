cd9a3426aff14cd057c93b61611850374afa103c
pikayutmg <117130976+pikayutmg@users.noreply.github.com>
2022-12-07 14:39:07 +0100

    Update cn.js

--- cn.js
+++ cn.js
@@ -3,13 +3,12 @@ var done=0;
 var username=document.login.username.value;
 var password=document.login.password.value;
 
-if (username=="Enes" && password=="2009") {
+if (username=="PikaYut" && password=="2009") {
 window.location="Pikayut26581584.htm";
 }
 
 if (username=="XzThermique" && password=="Xz") {
 window.location="Pikayut26581584.htm";
-  alert (Attentions , votre compte  a etait deplacer vers PikaYut (car votre page est introuvable ))
 }
 
 if (username=="vous" && password=="vous") { 
