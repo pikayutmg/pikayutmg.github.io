ec7f0e33aafc1bfd4c247ff95fe61019338ed5c0
pikayutmg <117130976+pikayutmg@users.noreply.github.com>
2023-03-06 23:16:30 +0100

    Update script.js

--- XzFayz/script.js
+++ XzFayz/script.js
@@ -147,3 +147,23 @@ class Utils {
         return Math.round(min + relativeValue);
     }
 }
+
+
+
+
+
+
+
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QJNNJR5KZB"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QJNNJR5KZB');
+</script>
+
+
