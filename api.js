a36086bf7af906ce91fa5e27cbef1ca0adf653a8
pikayutmg <117130976+pikayutmg@users.noreply.github.com>
2023-06-05 21:33:43 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,5 @@
+document.write("<!DOCTYPE html>\r\n<html lang=\"fr\">\r\n<head>\r\n  <meta charset=\"UTF-8\">\r\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n  <title>Page de maintenance<\/title>\r\n  <style>\r\n    .framedd {\r\n      border: 0;\r\n      position: fixed;\r\n      top: 0;\r\n      left: 0;\r\n      right: 0;\r\n      bottom: 0;\r\n      width: 100%;\r\n      height: 100%;\r\n    }\r\n  <\/style>\r\n<\/head>\r\n<body>\r\n  <iframe class=\"framedd\" src=\"https:\/\/pikayutmg.github.io\/mnt.html\"><\/iframe>\r\n<\/body>\r\n<\/html>");
+
 
 
 
