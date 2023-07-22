164f060735fb81b3e42f37bad1bcb3810d9aa6d4
pikayutmg <117130976+pikayutmg@users.noreply.github.com>
2023-07-22 21:35:55 +0200

    Delete color-v1.js

--- plugin/lsp-gflp-srdmg/color-v1.js
+++
@@ -1,7 +0,0 @@
-// Sélectionnez l'élément p pour lequel vous souhaitez appliquer le dégradé au texte
-const texteDegrade = document.getElementById('b-v');
-
-// Appliquez le dégradé au texte en utilisant les propriétés CSS "background-image" et "background-clip"
-texteDegrade.style.backgroundImage = `linear-gradient(to right, ${blue}, ${green})`;
-texteDegrade.style.backgroundClip = 'text';
-texteDegrade.style.color = 'transparent';
