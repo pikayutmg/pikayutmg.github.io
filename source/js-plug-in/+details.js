5cf7c1ea6640e9a269ee130ba58ae6b90e864740
pikayutmg <117130976+pikayutmg@users.noreply.github.com>
2023-12-11 21:48:47 +0100

    Update +details.js

--- source/js-plug-in/+details.js
+++ source/js-plug-in/+details.js
@@ -1,1 +1,17 @@
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QJNNJR5KZB"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QJNNJR5KZB');
+
+ 
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "gspjcqjaoi");
+ 
