f5ead6530eeeb810866cfc5294a7d889769cb944
pikayutmg <117130976+pikayutmg@users.noreply.github.com>
2023-12-21 22:13:10 +0100

    Update ui.js

--- web/ebsayder-roleplay/ebsayder-membre/js/user/here/ui.js
+++ web/ebsayder-roleplay/ebsayder-membre/js/user/here/ui.js
@@ -1,30 +1,32 @@
- 
-  // Fonction pour charger les données du profil depuis le fichier JSON
-  async function loadProfileData() {
-    try {
-      const response = await fetch('https://gflp-p.vercel.app/web/ebsayder-roleplay/ebsayder-membre/json/user/692098372747919480.json');
-      const userData = await response.json();
+// Fonction pour charger les données depuis le fichier JSON
+async function loadUserData() {
+  try {
+    const response = await fetch('https://gflp-p.vercel.app/web/ebsayder-roleplay/ebsayder-membre/json/user/692098372747919480.json');
+    const userData = await response.json();
 
-      // Sélectionnez les éléments où les informations du profil seront ajoutées
-      const nameElement = document.getElementById('name');
-      const titleElement = document.getElementById('title');
-      const idElement = document.getElementById('id');
-      const profileImageElement = document.getElementById('profile-image');
+    // Sélectionnez les éléments HTML où les données seront affichées
+    const nameElement = document.getElementById('name');
+    const idElement = document.getElementById('id');
+    const profileImageElement = document.getElementById('profile-image');
 
-      // Récupérez les données du profil depuis le JSON
-      const userProfileData = userData[0].categories['user-details'];
+    // Vérifiez si "user-details" existe dans les catégories
+    if (userData[0].categories['user-details']) {
+      const userDetailsData = userData[0].categories['user-details'];
 
-      // Mettez à jour le contenu des éléments HTML avec les informations du profil
-      nameElement.textContent = `Pseudo : ${userProfileData.Pseudo}`;
-      titleElement.textContent = `Titre : ${userProfileData.Titre}`;
-      idElement.textContent = `Identifiant : ${userProfileData.Identifiant}`;
-      profileImageElement.src = userProfileData.Profilescreen;  // Mettez à jour la source de l'image
+      // Mettez à jour le pseudo
+      nameElement.innerText = userDetailsData.Pseudo;
 
-    } catch (error) {
-      console.error('Erreur lors du chargement des données:', error);
+      // Mettez à jour l'identifiant
+      idElement.innerText = userDetailsData.Identifiant;
+
+      // Mettez à jour l'image de profil
+      profileImageElement.src = userDetailsData.Profilescreen;
+      profileImageElement.alt = userDetailsData.Pseudo; // Assurez-vous de donner une description pertinente pour l'accessibilité
     }
+  } catch (error) {
+    console.error('Erreur lors du chargement des données:', error);
   }
+}
 
-  // Appelez la fonction pour charger les données du profil
-  loadProfileData();
- 
+// Appelez la fonction pour charger les données
+loadUserData();
