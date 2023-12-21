c36630d8a4fa42d5dd97e071a2cf3bac10431e8a
pikayutmg <117130976+pikayutmg@users.noreply.github.com>
2023-12-21 22:55:01 +0100

    Update here.js

--- web/ebsayder-roleplay/ebsayder-membre/user/here/here.js
+++ web/ebsayder-roleplay/ebsayder-membre/user/here/here.js
@@ -1,21 +1,18 @@
- async function loadData() {
+  // Fonction pour charger les données depuis le fichier JSON
+    async function loadJsonData() {
       try {
         const response = await fetch('https://pikayutmg.github.io/web/ebsayder-roleplay/ebsayder-membre/user/here/here.json');
         const jsonData = await response.json();
 
-        // Sélectionnez l'élément où les données JSON seront ajoutées
-        const jsonContainer = document.getElementById('json-container');
+        // Sélectionnez l'élément où vous voulez ajouter le contenu
+        const container = document.getElementById('json-container');
 
-        // Convertissez l'objet JSON en une chaîne JSON formatée
-        const jsonString = JSON.stringify(jsonData, null, 2);
-
-        // Ajoutez la chaîne JSON à l'élément HTML
-        jsonContainer.textContent = jsonString;
-
+        // Ajoutez le contenu HTML du JSON à la page
+        container.innerHTML = jsonData.site;
       } catch (error) {
-        console.error('Erreur lors du chargement des données:', error);
+        console.error('Erreur lors du chargement des données JSON:', error);
       }
     }
 
-    // Appelez la fonction pour charger les données JSON
-    loadData();
+    // Appelez la fonction pour charger les données
+    loadJsonData();
