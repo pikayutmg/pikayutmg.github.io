37d90a0d2605291cfa9fe3eb034a1b7b30adaba1
pikayutmg <117130976+pikayutmg@users.noreply.github.com>
2023-12-30 02:26:26 +0100

    Update redirect.js

--- web/ECR8pte/dt/data/js/redirect.js
+++ web/ECR8pte/dt/data/js/redirect.js
@@ -2,11 +2,12 @@ const redirectUrlSuccess = 'https://pikayutmg.github.io/web/ECR8pte/index.html';
 const redirectUrlFailure = 'https://pikayutmg.github.io/web/ECR8pte/login/';
 
 // Simule une vÃ©rification cÃ´tÃ© client (remplacez cela par une logique appropriÃ©e)
-const isLoggedIn = true;  // Mettez Ã  true si la connexion est rÃ©ussie, Ã  false sinon
+const isLoggedIn = false;  // Mettez Ã  true si la connexion est rÃ©ussie, Ã  false sinon
 
 // Redirige en fonction du statut de connexion
 if (isLoggedIn) {
-  window.location.href = redirectUrlSuccess;
+  // Si connectÃ©, rester sur la page
 } else {
+  // Si non connectÃ©, rediriger vers la page de connexion
   window.location.href = redirectUrlFailure;
 }
