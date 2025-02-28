7176a8ab26dfb0341a59cc43df817e0c68b8049a
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-03-01 00:40:36 +0100

    Update A-all.js

--- script/A-all.js
+++ script/A-all.js
@@ -1,3 +1,14 @@
+//
+// Place Text from ``
+//
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("ecde_all").innerHTML = `
+<!--// Rien pour le moment-->
+    `;
+  });
+  //
+  // Replace Text from TB.json
+  //
     // Fonction pour charger le fichier JSON et remplacer les balises
     async function replaceTextFromJson() {
         try {
@@ -5,11 +16,11 @@
           const response = await fetch('https://enes-cde.vercel.app/data/json/TB.json');
           const jsonData = await response.json();
   
-          // Parcourir toutes les balises avec un id qui commence par "TB:"
-          const elements = document.querySelectorAll('[id^="TB:"]');
+          // Sélectionner tous les éléments avec un id spécifique
+          const elements = document.querySelectorAll('[id^="ecde_annee_set"]');
           
           elements.forEach((element) => {
-            const key = element.id.split(':')[1]; // Extraire la clé après "TB:"
+            const key = element.id; // La clé ici est l'id entier (ex : 'ecde_annee_set')
             
             // Vérifier si la clé existe dans le fichier JSON
             if (jsonData.hasOwnProperty(key)) {
@@ -19,6 +30,7 @@
               console.warn(`Clé "${key}" introuvable dans le fichier JSON.`);
             }
           });
+  
         } catch (error) {
           console.error("Erreur lors du chargement du fichier JSON:", error);
         }
