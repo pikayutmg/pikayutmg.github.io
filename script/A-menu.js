e1ccc763ee65e2131a1ab5568c443683d959d66f
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-03-02 03:48:21 +0100

    Update A-menu.js

--- script/A-menu.js
+++ script/A-menu.js
@@ -67,6 +67,12 @@ document.addEventListener("DOMContentLoaded", function () {
                     </svg>
                 </button>
             </a>
+
+            <a href="https://enes-cde.vercel.app/users/">
+                <button class="w-full p-3 flex justify-center rounded-lg text-gray-500 hover:bg-gray-50 dark:text-gray-400 dark:hover:bg-gray-800">
+                    <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M0 0h48v48H0z" fill="none"></path> <g id="Shopicon"> <path d="M31.278,25.525C34.144,23.332,36,19.887,36,16c0-6.627-5.373-12-12-12c-6.627,0-12,5.373-12,12 c0,3.887,1.856,7.332,4.722,9.525C9.84,28.531,5,35.665,5,44h38C43,35.665,38.16,28.531,31.278,25.525z M16,16c0-4.411,3.589-8,8-8 s8,3.589,8,8c0,4.411-3.589,8-8,8S16,20.411,16,16z M24,28c6.977,0,12.856,5.107,14.525,12H9.475C11.144,33.107,17.023,28,24,28z"></path> </g> </g></svg>
+                </button>
+            </a>
         </nav>
     </div>
 </aside>
