24ef7d275962c5c2fe90e9ad5a84c06473c8bd88
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-03-03 12:31:14 +0100

    Update A-new_update.js

--- script/A-new_update.js
+++ script/A-new_update.js
@@ -11,3 +11,16 @@ document.addEventListener("DOMContentLoaded", function () {
     `;
   });
 
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("latest2_enescde").innerHTML = `
+<div>
+                      <h3 class="text-lg font-semibold text-gray-900 mb-4">Lastest : </h3>
+                      <div class="bg-gray-100 p-4 rounded-lg">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Bundesarchiv_Bild_183-H1216-0500-002%2C_Adolf_Hitler.jpg/220px-Bundesarchiv_Bild_183-H1216-0500-002%2C_Adolf_Hitler.jpg" alt="Featured Product" class="rounded-lg mb-3">
+                        <h4 class="font-medium text-gray-900">Nouvelle annonce : </h4>
+                        <p class="text-sm text-gray-600 mb-2">Nouvelle versions PKYT - Account  V3dd > Enes-CDE <b>v4</b>.</p>
+                        <a href="https://enes-cde.vercel.app/depot" class="text-indigo-600 hover:text-indigo-800 text-sm font-medium">Commencer →</a>
+                      </div>
+    `;
+  });
+
