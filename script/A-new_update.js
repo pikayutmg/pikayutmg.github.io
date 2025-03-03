0efb6dfa62aaee2f95e782b921dcdf9838b73ae0
PKYT-Service <164098247+PKYT-Service@users.noreply.github.com>
2025-03-03 12:27:53 +0100

    Update A-new_update.js

--- script/A-new_update.js
+++ script/A-new_update.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
 <div>
                       <h3 class="text-lg font-semibold text-gray-900 mb-4">Lastest : </h3>
                       <div class="bg-gray-100 p-4 rounded-lg">
-                        <img src="https://images.unsplash.com/photo-1546435770-a3e426bf472b?q=80&w=2065&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Featured Product" class="rounded-lg mb-3">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Bundesarchiv_Bild_183-H1216-0500-002%2C_Adolf_Hitler.jpg/220px-Bundesarchiv_Bild_183-H1216-0500-002%2C_Adolf_Hitler.jpg" alt="Featured Product" class="rounded-lg mb-3">
                         <h4 class="font-medium text-gray-900">Nouvelle annonce : </h4>
-                        <p class="text-sm text-gray-600 mb-2">Nouvelle versions PKYT - Account  V3 > Enes-CDE <b>v4</b>.</p>
+                        <p class="text-sm text-gray-600 mb-2">Nouvelle versions PKYT - Account  V3dd > Enes-CDE <b>v4</b>.</p>
                         <a href="https://enes-cde.vercel.app/depot" class="text-indigo-600 hover:text-indigo-800 text-sm font-medium">Commencer →</a>
                       </div>
     `;
